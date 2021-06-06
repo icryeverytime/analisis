db22a1ada03bc88fe9aff9a6e5be908180529a84
icryeverytime <al242348@edu.uaa.mx>
2021-06-05 17:44:02 -0700

    Se agrego generar la factura

---
+++ generarFactura.php
@@ -0,0 +1,66 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+$contra="123";
+$correo="christianantonio12322@gmail.com";
+
+
+$currentdate=date('Y-m-d');
+$regimenfiscal="regimen";
+$codigobarras="codigobarras";
+$serieemisor="342234";
+$seriesat="698712";
+$leyendafinal="hola";
+$cadenasat="44";
+
+
+$conn = new mysqli($servername, $username, $password,$dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT idcliente FROM Cliente WHERE (contra='$contra' AND correo='$correo')";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+        $id=$row["idcliente"]; 
+  }
+} else {
+  echo "1. 0 results<br>";
+}
+
+$sql = "SELECT num_paq FROM Servicio WHERE (id_cuenta='$id')";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+        $num_paq=$row["num_paq"]; 
+  }
+} else {
+  echo "0 results";
+}
+
+$sql = "SELECT fechacontratacion FROM Servicio WHERE (id_cuenta='$id')";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+        $primerafecha=$row["fechacontratacion"]; 
+  }
+} else {
+  echo "0 results";
+}
+
+$periodo=$primerafecha." a ".$currentdate;
+
+$sql="INSERT INTO Factura(num_paqFAC,id_cuentaFAC,regimenfiscal,periodo,codigobarras,serieCSDDemisor,serieCSDDsat,leyendafinal,fechayhorafactura,cadenaSAT)
+VALUES ('$num_paq','$id','$regimenfiscal','$periodo','$codigobarras','$serieemisor','$seriesat','$leyendafinal','$currentdate','$cadenasat');";
+if ($conn->query($sql) === TRUE) {
+    echo "insertado correctamente";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+$conn->close();
+?>
